516468df00abd418e2dfa56a7f5bec903c05a375
Jan Järfalk <jan@jarfalk.se>
2016-05-10 17:55:36 +0200

    test names

--- test/getPermutationsSpec.js
+++ test/getPermutationsSpec.js
@@ -3,12 +3,12 @@ import assert from 'assert';
 import getPermutations from '../src/getPermutations.js';
 
 describe('getPermutations() should...', function () {
-    it(`find diff in shallow object`, function () {
+    it(`perumtate array with chars`, function () {
         const permutations = getPermutations(['a','b']);
         assert.deepEqual(permutations, [['b','a'],['a','b']]);
     });
 
-    it(`find diff in shallow object`, function () {
+    it(`perumtate array with numbers`, function () {
         const permutations = getPermutations([1,0,1]);
         assert.deepEqual(permutations, [[0,1,1],[0,1,1],[1,0,1],[1,1,0],[1,1,0],[1,0,1]]);
     });
